444e977a95202aabedf9dbae62d7bfb59fb6c376
Bekosprivate <itsmybeko@gmail.com>
2026-04-28 20:07:43 +0300

    model degistirildi

--- bot.py
+++ bot.py
@@ -189,7 +189,7 @@ def chat_with_groq(user_id, user_message):
 
     while True:
         response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
+            model="mixtral-8x7b-32768",
             messages=messages,
             tools=TOOLS,
             tool_choice="auto",
